d24faa3fee8d4208c77ccc281e2d8600e1c72d07
hameed <40880601+hamsyed@users.noreply.github.com>
2021-05-29 22:43:41 +1000

    Update launchconfiguration.tf

--- launchconfiguration.tf
+++ launchconfiguration.tf
@@ -1,7 +1,7 @@
 resource "aws_launch_configuration" "asg-launch-webserver" {
   image_id          = "${var.ami}"
   instance_type = "${var.instance_type}"
-  key_name = "newkey"
+  key_name = "terraform"
   security_groups = ["${aws_security_group.webserver_sg.id}"]
   
   user_data = <<-EOF
